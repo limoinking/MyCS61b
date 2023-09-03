8cb2e970de5c7c2c7985a9c97a842f51fe3f2741
limoinking <a292854559@163.com>
2023-09-03 22:53:24 +0800

    111

--- lab2/DebugExercise/DebugExercise1.java
+++ lab2/DebugExercise/DebugExercise1.java
@@ -1,10 +1,12 @@
+package DebugExercise;
+
 /**
  * Exercise for learning how the debug, breakpoint, and step-into
  * feature work.
  */
 public class DebugExercise1 {
     public static int divideThenRound(int top, int bottom) {
-        float quotient = (float) top / bottom;
+        int quotient = top / bottom;
         int result = Math.round(quotient);
         return result;
     }
